8933cb3f3098eeb68002cc333491fd5262ba74e5
Eric Prestat <eric.prestat@gmail.com>
2021-11-12 18:08:38 +0000

    Add mkl and nomkl variant on linux64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,8 +1,10 @@
-(
-echo [mkl]
-echo library_dirs = %LIBRARY_LIB%
-echo include_dirs = %LIBRARY_INC%
-echo libraries = mkl_intel_lp64, mkl_gf_lp64, mkl_intel_thread, mkl_core, mkl_rt, iomp5
-) > site.cfg
+if "%mkl_support%" == "mkl" (
+  (
+  echo [mkl]
+  echo library_dirs = %LIBRARY_LIB%
+  echo include_dirs = %LIBRARY_INC%
+  echo libraries = mkl_intel_lp64, mkl_gf_lp64, mkl_intel_thread, mkl_core, mkl_rt, iomp5
+  ) > site.cfg
+)
 
 %PYTHON% -m pip install . --no-deps -vv
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+if [[ $mkl_support == "mkl" ]]; then
 cat > site.cfg <<EOF
 [mkl]
 library_dirs = $PREFIX/lib
 include_dirs = $PREFIX/include
 libraries = mkl_intel_lp64, mkl_gf_lp64, mkl_intel_thread, mkl_core, mkl_rt, iomp5
 EOF
+echo "#####################"
+echo "site.cfg file written"
+fi
 
 $PYTHON -m pip install . --no-deps -vv
