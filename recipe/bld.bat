392032efa1402fe00ef74e1bc115c2011324e9ce
Eric Prestat <eric.prestat@gmail.com>
2021-11-12 19:42:49 +0000

    Add mkl and nomkl variant on linux64

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -3,8 +3,8 @@ if "%mkl_support%" == "mkl" (
   echo [mkl]
   echo library_dirs = %LIBRARY_LIB%
   echo include_dirs = %LIBRARY_INC%
-  echo libraries = mkl_intel_lp64, mkl_gf_lp64, mkl_intel_thread, mkl_core, mkl_rt, iomp5
+  echo libraries = mkl_intel_lp64, mkl_intel_thread, mkl_core, libiomp5md
   ) > site.cfg
 )
 
-%PYTHON% -m pip install . --no-deps -vv
+%PYTHON% setup.py install
