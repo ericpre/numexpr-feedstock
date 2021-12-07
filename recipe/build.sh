7edecea9fe61291f96999b49a5624dfc47574d28
Eric Prestat <eric.prestat@gmail.com>
2021-12-07 18:20:09 +0000

    Update to 2.8.0

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ cat > site.cfg <<EOF
 [mkl]
 library_dirs = $PREFIX/lib
 include_dirs = $PREFIX/include
-libraries = mkl_intel_lp64, mkl_gf_lp64, mkl_intel_thread, mkl_core, mkl_rt, iomp5
+libraries = mkl_rt
 EOF
 echo "#####################"
 echo "site.cfg file written"
